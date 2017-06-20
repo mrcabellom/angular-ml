179a97be777de94253d8bdab15811cd905dc293d
Rodrigo Cabello <mrcabello@plainconcepts.com>
2017-06-20 13:01:19 +0200

    Adding styles to chemotherapy cycles

--- server/treatment/generateTreatment.ts
+++
@@ -1,6 +0,0 @@
-import { Treatment } from './treatment';
-import * as fs from 'fs';
-
-let treatment = new Treatment(100, 187, new Date())
-let generated = treatment.generateTreatment();
-fs.writeFile('treatment_generated.json',JSON.stringify(generated));
